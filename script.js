86ef5819729e5c066d34d2a4b335962e0471a87e
Ana <ana.lucia.villalobos.rivas@gmail.com>
2025-02-21 13:19:09 -0600

    Agregando la sección de JavaScript

---
+++ script.js
@@ -0,0 +1,55 @@
+function colorAndy() {
+    const colores = ["Green", "Blue", "Red"];
+    const aleatorio = Math.floor(Math.random() * colores.length);
+    document.getElementById("andy").style.color = colores[aleatorio];
+    console.log(colores[aleatorio]);
+}
+
+function colorStep() {
+    const colores = ["Green", "Blue", "Red"];
+    const aleatorio = Math.floor(Math.random() * colores.length);
+    document.getElementById("step").style.color = colores[aleatorio];
+    console.log(colores[aleatorio]);
+}
+
+function colorBrenn() {
+    const colores = ["Green", "Blue", "Red"];
+    const aleatorio = Math.floor(Math.random() * colores.length);
+    document.getElementById("brenn").style.color = colores[aleatorio];
+    console.log(colores[aleatorio]);
+}
+
+function colorAnita() {
+    const colores = ["Green", "Blue", "Red"];
+    const aleatorio = Math.floor(Math.random() * colores.length);
+    document.getElementById("anita").style.color = colores[aleatorio];
+    console.log(colores[aleatorio]);
+}
+
+function colorItzi() {
+    const colores = ["Green", "Blue", "Red"];
+    const aleatorio = Math.floor(Math.random() * colores.length);
+    document.getElementById("itzi").style.color = colores[aleatorio];
+    console.log(colores[aleatorio]);
+}
+
+function colorAtli() {
+    const colores = ["Green", "Blue", "Red"];
+    const aleatorio = Math.floor(Math.random() * colores.length);
+    document.getElementById("atli").style.color = colores[aleatorio];
+    console.log(colores[aleatorio]);
+}
+
+function colorErika() {
+    const colores = ["Green", "Blue", "Red"];
+    const aleatorio = Math.floor(Math.random() * colores.length);
+    document.getElementById("erika").style.color = colores[aleatorio];
+    console.log(colores[aleatorio]);
+}
+
+function colorEdna() {
+    const colores = ["Green", "Blue", "Red"];
+    const aleatorio = Math.floor(Math.random() * colores.length);
+    document.getElementById("edna").style.color = colores[aleatorio];
+    console.log(colores[aleatorio]);
+}
